a3871c84ebd3b7bfedfb6f014031f6da58bfebb0
Jimmy Kamau <jimmy.kamau@andela.com>
2017-10-04 11:26:39 +0300

    [Feature 151670894] Set up manage script

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
 
 
 class ProductionConfig(object):
